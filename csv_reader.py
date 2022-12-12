33b73cab8faa82aaec720046d60268b31926b6d9
Klaus8k <klaus888@yandex.ru>
2022-12-12 22:38:03 +0200

    1

--- csv_reader.py
+++ csv_reader.py
@@ -1,5 +1,12 @@
 import pathlib
 
-x = pathlib.Path()
+x = pathlib.Path.cwd()
+file_name =  '*.csv'
+files_dir = pathlib.Path.home() / 'www' / 'my_instruments'
+target_file = x / 'csv_files/01.01.2022-12.12.2022.csv'
 
-print(x)
+with open(target_file, 'r') as file:
+    count = 0
+    for i in file:
+        count += 1
+        print(count)
