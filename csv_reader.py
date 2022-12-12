34120af02b26b89c5d066584da5236f3e190c33a
Klaus8k <klaus888@yandex.ru>
2022-12-12 23:01:57 +0200

    1111

--- csv_reader.py
+++ csv_reader.py
@@ -5,8 +5,7 @@ file_name =  '*.csv'
 files_dir = pathlib.Path.home() / 'www' / 'my_instruments'
 target_file = x / 'csv_files/01.01.2022-12.12.2022.csv'
 
-with open(target_file, 'r') as file:
-    count = 0
-    for i in file:
-        count += 1
-        print(count)
+i = target_file.read_text()
+
+list_target_file_csv = i.split('\n')
+
