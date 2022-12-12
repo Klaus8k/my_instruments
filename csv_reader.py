ff1dbdf0e0d6551f38ee63627aec10f5f7a76306
Klaus8k <klaus888@yandex.ru>
2022-12-13 00:05:57 +0200

    123

--- csv_reader.py
+++ csv_reader.py
@@ -9,15 +9,26 @@ file_name =  '*.csv'
 files_dir = pathlib.Path.home() / 'www' / 'my_instruments'
 target_file = x / 'csv_files/01.01.2022-12.12.2022.csv'
 
+def print_dict(dict_from_csv: dict):
+    for i in dict_from_csv.keys():
+        print(i, '---->', dict_from_csv[i])
+
 with open(target_file, 'r') as file:
     reader = csv.DictReader(file, delimiter=';')
-    print(reader.fieldnames)
 
     sum = 0
+
+    result_dict = {}
     for i in reader:
+        cash = int(i['Сумма в валюте счета'].split(',')[0])
+        if i['Тип операции (пополнение/списание)'] == 'Debit' and \
+                            i['Категория операции'] == 'cardOperation':
 
-        if i['Тип операции (пополнение/списание)'] == 'Debit':
-            sum += int(i['Сумма в валюте счета'].split(',')[0])
-            print(i['Дата транзакции'], str(i['Сумма в валюте счета']))
+            if i['Контрагент'] not in result_dict.keys():
+                result_dict[i['Контрагент']] = cash
+            else:
+                result_dict[i['Контрагент']] += cash
 
-    print(sum)
+
+
+    print_dict(result_dict)
