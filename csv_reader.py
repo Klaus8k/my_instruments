67caf5f11f57e6d46f91a5b14b6ea5930d3b1ab4
Oleg Sokolov <klaus888@yandex.ru>
2022-12-13 18:59:42 +0300

    csv parse

--- csv_reader.py
+++ csv_reader.py
@@ -4,16 +4,28 @@ import logging
 
 logger = logging.basicConfig(filename='log_csv.txt', filemode='a')
 
-x = pathlib.Path.cwd()
-file_name =  '*.csv'
-files_dir = pathlib.Path.home() / 'www' / 'my_instruments'
-target_file = x / 'csv_files/01.01.2022-12.12.2022.csv'
+files_dir = pathlib.Path.cwd() / 'csv_files'
+target_file = files_dir / '01.01.2022-12.12.2022.csv'
+
+print(target_file)
 
 def print_dict(dict_from_csv: dict):
-    for i in dict_from_csv.keys():
-        print(i, '---->', dict_from_csv[i])
+    sorted_dict = {}
+    sorted_keys = sorted(dict_from_csv, key=dict_from_csv.get)
 
-with open(target_file, 'r') as file:
+    suming = 0
+    for i in sorted_keys:
+        sorted_dict[i] = dict_from_csv[i]
+        print(i, '------', sorted_dict[i])
+        suming += sorted_dict[i]
+
+    print(suming)
+
+
+
+
+
+with open(target_file, 'r', encoding='utf8') as file:
     reader = csv.DictReader(file, delimiter=';')
 
     sum = 0
@@ -23,11 +35,15 @@ with open(target_file, 'r') as file:
         cash = int(i['Сумма в валюте счета'].split(',')[0])
         if i['Тип операции (пополнение/списание)'] == 'Debit' and \
                             i['Категория операции'] == 'cardOperation':
+                                   
 
-            if i['Контрагент'] not in result_dict.keys():
-                result_dict[i['Контрагент']] = cash
+            if i['Контрагент'].rstrip() not in result_dict.keys():
+
+
+                result_dict[i['Контрагент'].rstrip()] = cash
             else:
-                result_dict[i['Контрагент']] += cash
+                result_dict[i['Контрагент'].rstrip()] += cash
+
 
 
 
