aeaa9cc21fbdd0fc069f58f1670ccf17797fbe6f
Oleg Sokolov <klaus888@yandex.ru>
2022-12-21 18:52:59 +0300

    1

--- types_lesson.py
+++ types_lesson.py
@@ -1,3 +1,31 @@
-fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'bananaz']
-a = fruits[:]
-print(a[-1:])
+# put your python code here
+# a = input()
+a = '()[]}'
+a = list(a)
+
+match = {'(': ')',
+         '[': ']',
+         '{': '}'}
+
+
+def match_brakes(stack):
+    b = []
+    count = 1
+    for i in stack:
+        count += 1
+
+        print(i)
+        if i in match.keys():
+            b.append(i)
+        elif i in match.values():
+            for j in match.keys():
+                if match[j] == i and j in b:
+                    b.remove(j)
+                else: break
+            return count # С возвратом разобраться
+        else:
+            continue
+
+
+
+print(match_brakes(a))
