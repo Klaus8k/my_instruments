84da93af00f8d902e685541ece7125d6ab5ef0f2
Klaus8k <klaus888@yandex.ru>
2022-12-27 22:28:44 +0200

    111

--- test.py
+++ test.py
@@ -1,6 +1,40 @@
-a = 10
-b = 2
+import logging
 
-a,b = b,a
+logger = logging.getLogger('timer')
+exit_handler = logging.StreamHandler()
+format_formatter = logging.Formatter('%(asctime)s --- %(message)s')
+exit_handler.setFormatter(format_formatter)
+logger.addHandler(exit_handler)
 
-print (a,b)
+import sys
+sys.setrecursionlimit(20000)
+
+def timer(fn):
+    def wrap(*args, **kwargs):
+        logger.warning('start')
+        fn(*args, **kwargs)
+        logger.warning('end')
+    return wrap
+
+def fuctor(x):
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return x * fuctor(x-1)
+
+logger.warning('start')
+fuctor(2000)
+logger.warning('end====================')
+
+
+@timer
+def fuc(l):
+    res = 1
+    x = 1
+    while x <= l:
+        res *= x
+        x += 1
+
+    return res
+
+print(fuc(2000))
